7d7d6da6bd2beefcb9decb88be0ce87865446d44
alwaysdumped <schetan24021955@gmail.com>
2025-08-19 23:19:27 +0530

    updated contact and added styles

---
+++ website/src/Contact.jsx
@@ -0,0 +1,37 @@
+//placeholder 
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="contact-container" style={{ padding: "90px 1rem 3rem"}}>
+      <div className="contact-content">
+        <div className="contact-text">
+          <h1 className="contact-tagline">
+            WE DO THIS<br />
+            NOT<br />
+            BECAUSE<br />
+            IT IS EASY<br />
+            BUT<br />
+            BECAUSE WE<br />
+            THOUGHT IT<br />
+            WOULD BE<br />
+            EASY
+          </h1>
+        </div>
+        <div className="image-placeholders">
+          <div className="image-placeholder">
+            <span>Image 1</span>
+          </div>
+          <div className="image-placeholder">
+            <span>Image 2</span>
+          </div>
+          <div className="image-placeholder">
+            <span>Image 3</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
